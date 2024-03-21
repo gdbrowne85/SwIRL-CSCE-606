cc422ac6f4f2693a7e4c7de683cef196b459d111
ermcgonagle <ermcgonagle>
2024-03-21 13:20:24 -0500

    Reminder email sends when timer runs out intial functionality

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="countdown"
 export default class extends Controller {
@@ -6,6 +6,8 @@ export default class extends Controller {
 
   connect() {
     console.log("Connected");
+
+    this.eventID = this.element.dataset.eventId;
 
     this.secondsUntilEnd = this.countdownTarget.dataset.secondsUntilEndValue;
 
@@ -19,9 +21,11 @@ export default class extends Controller {
     const now = new Date();
     const secondsRemaining = (this.endTime - now) / 1000;
 
-    if (secondsRemaining <= 0){
+    if (secondsRemaining <= 0) {
       clearInterval(this.countdown);
       this.countdownTarget.innerHTML = "Send Reminder Email";
+
+      this.inviteAttendees(this.eventID);
       return;
     }
 
@@ -29,11 +33,27 @@ export default class extends Controller {
     const secondsPerHour = 3600;
     const secondsPerMinute = 60;
 
-    const days = Math.floor(secondsRemaining/secondsPerDay);
-    const hours = Math.floor((secondsRemaining%secondsPerDay)/secondsPerHour);
-    const minutes = Math.floor((secondsRemaining%secondsPerHour)/secondsPerMinute);
-    const seconds = Math.floor(secondsRemaining%secondsPerMinute);
+    const days = Math.floor(secondsRemaining / secondsPerDay);
+    const hours = Math.floor((secondsRemaining % secondsPerDay) / secondsPerHour);
+    const minutes = Math.floor((secondsRemaining % secondsPerHour) / secondsPerMinute);
+    const seconds = Math.floor(secondsRemaining % secondsPerMinute);
 
     this.countdownTarget.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  }
+
+  inviteAttendees(eventID) {
+    // Make AJAX request to call invite_attendees function
+    fetch(`/invite_attendees/${eventID}`, {
+      method: 'GET',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to invite attendees');
+        }
+        console.log('Invite attendees successfully');
+      })
+      .catch(error => {
+        console.error('Error inviting attendees:', error);
+      });
   }
 }
