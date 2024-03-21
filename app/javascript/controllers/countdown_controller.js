6cc31bd76b5acf1c067fd93485d74f2238b5245b
debalgoswami <debal.goswami00@gmail.com>
2024-03-21 10:04:19 -0500

    Bringing back old changes

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -6,37 +6,34 @@ export default class extends Controller {
 
   connect() {
     console.log("Connected");
-  }
 
-  startCountdown(secondsUntilEnd) {
+    this.secondsUntilEnd = this.countdownTarget.dataset.secondsUntilEndValue;
+
     const now = new Date().getTime();
-    const endTime = new Date(now + secondsUntilEnd * 1000);
+    this.endTime = new Date(now + this.secondsUntilEnd * 1000);
 
-    const countdown = setInterval(() => {
-      const now = new Date();
-      const secondsRemaining = (endTime - now) / 1000;
-
-      if (secondsRemaining <= 0){
-        clearInterval(countdown);
-        this.countdownTarget.innerHTML = "Reminder Time";
-        return;
-      }
-
-      const secondsPerDay = 86400;
-      const secondsPerHour = 3600;
-      const secondsPerMinute = 60;
-
-      const days = Math.floor(secondsRemaining/secondsPerDay);
-      const hours = Math.floor((secondsRemaining%secondsPerDay)/secondsPerHour);
-      const minutes = Math.floor((secondsRemaining%secondsPerHour)/secondsPerMinute);
-      const seconds = Math.floor(secondsRemaining%secondsPerMinute);
-
-      this.countdownTarget.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
-    }, 1000);
+    this.countdown = setInterval(this.countdown.bind(this), 1000);
   }
 
-  sendEmails() {
-    const secondsUntilEnd = this.countdownTarget.dataset.secondsUntilEndValue;
-    this.startCountdown(secondsUntilEnd);
+  countdown() {
+    const now = new Date();
+    const secondsRemaining = (this.endTime - now) / 1000;
+
+    if (secondsRemaining <= 0){
+      clearInterval(this.countdown);
+      this.countdownTarget.innerHTML = "Send Reminder Email";
+      return;
+    }
+
+    const secondsPerDay = 86400;
+    const secondsPerHour = 3600;
+    const secondsPerMinute = 60;
+
+    const days = Math.floor(secondsRemaining/secondsPerDay);
+    const hours = Math.floor((secondsRemaining%secondsPerDay)/secondsPerHour);
+    const minutes = Math.floor((secondsRemaining%secondsPerHour)/secondsPerMinute);
+    const seconds = Math.floor(secondsRemaining%secondsPerMinute);
+
+    this.countdownTarget.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
   }
 }
