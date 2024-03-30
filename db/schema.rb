6945a8bb8bc84fa8a0247b07adf488ae099fe904
debalgoswami <debal.goswami00@gmail.com>
2024-03-29 19:08:33 -0500

    Changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_07_050749) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_25_202614) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_07_050749) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_07_050749) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_07_050749) do
     t.string "email_token"
     t.boolean "email_sent", default: false
     t.datetime "email_sent_time"
+    t.boolean "reminder_email_sent", default: false
     t.index ["email_token"], name: "index_attendee_infos_on_email_token", unique: true
   end
 
