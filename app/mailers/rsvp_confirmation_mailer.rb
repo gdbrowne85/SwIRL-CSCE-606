5fbc4512b30d0eb5d1b3fe527d651c0e0a8ad016
debalgoswami <debal.goswami00@gmail.com>
2024-04-04 17:52:41 -0500

    rsvp_confirmation_mailer

--- app/mailers/rsvp_confirmation_mailer.rb
+++ app/mailers/rsvp_confirmation_mailer.rb
@@ -1,4 +1,8 @@
 # app/mailers/invitation_confirmation_mailer.rb
+require 'roo'
+require 'active_support/all'
+include CalendarHelper
+
 class RsvpConfirmationMailer < ApplicationMailer
     default from: 'SkheduleSp24@gmail.com'
     default_url_options[:host] = 'https://swirlskehdule-f316b598c688.herokuapp.com/'
@@ -6,15 +10,17 @@ class RsvpConfirmationMailer < ApplicationMailer
     def acceptance_email(inviter_email, event_name)
       @inviter_email = inviter_email
       @event_name = event_name
+      @url = 'https://swirlskehdule-f316b598c688.herokuapp.com/'
   
-      mail(to: @inviter_email, subject: 'Confirmation: User Joined Your Event')
+      mail(to: @inviter_email, subject: 'Confirmation: User Joined Your Event', template_name: 'email_invitation')
     end
 
     def rejection_email(inviter_email, event_name)
         @inviter_email = inviter_email
         @event_name = event_name
+        @url = 'https://swirlskehdule-f316b598c688.herokuapp.com/'
     
-        mail(to: @inviter_email, subject: 'Rejection: User Declined Your Event')
+        mail(to: @inviter_email, subject: 'Rejection: User Declined Your Event', template_name: 'email_invitation')
       end
   end
   
