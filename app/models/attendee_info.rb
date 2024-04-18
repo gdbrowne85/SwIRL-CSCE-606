a2a866a01502141efd4a994381876fb97f0189f1
Werh4N <werhan0924@gmail.com>
2024-04-17 23:32:31 -0500

    change email status

--- app/models/attendee_info.rb
+++ app/models/attendee_info.rb
@@ -3,4 +3,17 @@
 class AttendeeInfo < ApplicationRecord
   belongs_to :event
   has_one :time_slot, required: false
+
+  enum status: {
+    not_sent: 0,
+    awaiting_reply: 1,
+    replied_attending: 2,
+    replied_not_attending: 3
+  }, _prefix: true
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+  def set_default_status
+    self.status ||= :awaiting_reply
+  end
 end
