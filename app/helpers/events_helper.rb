dd7664d52cbb9711b9553f00dd8e863f260ea905
Carlos Meisel <carlosmei33@outlook.com>
2024-04-30 00:13:45 -0500

    Revert to PR59 controllers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,28 +4,28 @@
 
 module EventsHelper
   def readable_status(status)
-    case status
-    when 'awaiting_reply'
-      'Invitation sent - awaiting reply'
-    when 'replied_attending'
-      'Replied - attending'
-    when 'replied_not_attending'
-      'Replied - not attending'
-    else
-      'Unknown Status'
+      case status
+      when 'awaiting_reply'
+        'Invitation sent - awaiting reply'
+      when 'replied_attending'
+        'Replied - attending'
+      when 'replied_not_attending'
+        'Replied - not attending'
+      else
+        'Unknown Status'
+      end
     end
-  end
-
-  def status_class(status)
-    case status
-    when 'awaiting_reply'
-      'status-awaiting-reply'
-    when 'replied_attending'
-      'status-attending'
-    when 'replied_not_attending'
-      'status-not-attending'
-    else
-      'status-unknown' # Optionally handle unknown or default cases
+  
+    def status_class(status)
+      case status
+      when 'awaiting_reply'
+        'status-awaiting-reply'
+      when 'replied_attending'
+        'status-attending'
+      when 'replied_not_attending'
+        'status-not-attending'
+      else
+        'status-unknown' # Optionally handle unknown or default cases
+      end
     end
-  end
 end
